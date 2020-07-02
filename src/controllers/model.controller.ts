5f29b276d90e9089e127283c57e69d4eca29c1c9
Raúl Alexander Mendoza Muñoz <wazzu_3000@hotmail.com>
2020-07-01 20:06:24 -0500

    Fix some bugs

--- src/controllers/model.controller.ts
+++ src/controllers/model.controller.ts
@@ -7,130 +7,137 @@ import { Rules } from './../models/model';
 const defaultPageSize = '100';
 
 export class ModelController extends Controller {
-    private readonly model: Model<any>;
-    private readonly rules: Rules;
+  private readonly model: Model<any>;
+  private readonly rules: Rules;
 
-    public constructor(models: Dictionary<Model<any>>, rulesCollection: Dictionary<Rules>, user: Dictionary<any>, modelName: string) {
-        super(models, user);
-        this.model = models[modelName];
-        this.rules = rulesCollection[modelName];
+  public constructor(models: Dictionary<Model<any>>, rulesCollection: Dictionary<Rules>, user: Dictionary<any>, modelName: string) {
+    super(models, user);
+    this.model = models[modelName];
+    this.rules = rulesCollection[modelName];
+  }
+
+  /**
+   * Make a query from the specific collection, you can filter values with
+   * the querystring but the variables `_page`, `_page_size`, `_order_by`
+   * and `_order_by_desc` are used to alter the query.
+   * @param req The request sended by the client
+   * @param res The response send to the client
+   */
+  public async get(req: express.Request, res: express.Response)
+  /**
+   * Find a document by id form the specific collection
+   * @param req The request sended by the client
+   * @param res The response send to the client
+   * @param _id Id of the document
+   */
+  public async get(req: express.Request, res: express.Response, _id: any)
+  public async get(req: express.Request, res: express.Response, _id: any = undefined) {
+    if (_id) {
+      const getOne = this.rules.access.getOne;
+      const filter = {
+        _id,
+        delete: this.rules.logicalDelete ? false : undefined
+      }
+      if (typeof getOne === 'object') {
+        filter[getOne.filter.documentField] = this.user[getOne.filter.variableSession];
+      }
+
+      try {
+        const _res = await this.model.findOne(filter);
+        _res ? res.json(_res) : res.status(404).end();
+      } catch (err) {
+        res.status(500).end(err.message || err);
+      }
+    } else {
+      const filter = JSON.parse(JSON.stringify(req.query));
+      const getMany = this.rules.access.getMany;
+      const page = parseInt((typeof req.query._page === 'string' && req.query._page) || '1')
+      const pageSize = parseInt((typeof req.query._page_size === 'string' && req.query._page_size) || defaultPageSize)
+      delete filter._page;
+      delete filter._page_size;
+      delete filter._order_by;
+      delete filter._order_by_desc;
+      if (typeof getMany === 'object') {
+        filter[getMany.filter.documentField] = this.user[getMany.filter.variableSession];
+      }
+      if (this.rules.logicalDelete) {
+        filter.delete = false;
+      }
+
+      try {
+        const _res = await this.model.find(filter)
+          .skip(pageSize * (page - 1))
+          .limit(pageSize);
+
+        res.json(_res);
+      } catch(err) {
+        res.status(500).end(err.message || err)
+      }
+    }
+  }
+
+  /**
+   * Insert a document in the specific collection
+   * @param req The request sended by the client
+   * @param res The response send to the client
+   */
+  public post(req: express.Request, res: express.Response) {
+    const payload = { ...req.body };
+    const post = this.rules.access.post;
+    if (typeof post === 'object') {
+      payload[post.filter.documentField] = this.user[post.filter.variableSession];
+    }
+    if (this.rules.logicalDelete) {
+      payload.delete = false;
+    }
+    this.model.create(payload)
+      .then(_res => res.status(201).json(_res._id))
+      .catch(err => err => res.status(500).end(err.message || err));
+  }
+
+  /**
+   * Update a document in the specifict collection
+   * @param req The request sended by the client
+   * @param res The response send to the client
+   * @param _id Id of the document
+   */
+  public put(req: express.Request, res: express.Response, _id: any) {
+    const filter: Dictionary<any> = { _id };
+    const payload = { ...req.body };
+    const put = this.rules.access.put;
+    if (typeof put === 'object') {
+      delete payload[put.filter.documentField];
+      filter[put.filter.documentField] = this.user[put.filter.variableSession];
+    }
+    if (this.rules.logicalDelete) {
+      filter.delete = false;
+      payload.delete = false;
     }
 
-    /**
-     * Make a query from the specific collection, you can filter values with
-     * the querystring but the variables `_page`, `_page_size`, `_order_by`
-     * and `_order_by_desc` are used to alter the query.
-     * @param req The request sended by the client
-     * @param res The response send to the client
-     */
-    public get(req: express.Request, res: express.Response)
-    /**
-     * Find a document by id form the specific collection
-     * @param req The request sended by the client
-     * @param res The response send to the client
-     * @param _id Id of the document
-     */
-    public get(req: express.Request, res: express.Response, _id: any)
-    public get(req: express.Request, res: express.Response, _id: any = undefined) {
-        if (_id) {
-            const getOne = this.rules.access.getOne;
-            const filter = {
-                _id,
-                delete: this.rules.logicalDelete ? false : undefined
-            }
-            if (typeof getOne === 'object') {
-                filter[getOne.filter.documentField] = this.user[getOne.filter.variableSession];
-            }
+    this.model.update(
+      filter,
+      { $set: payload }
+    ).then(() => res.status(204).end())
+    .catch(err => res.status(500).end(err.message || err));
+  }
 
-            this.model.findOne(filter)
-                .then(_res => _res ? res.json(_res) : res.status(404).end())
-                .catch(err => res.status(500).end(err.message || err));
-        } else {
-            const filter = JSON.parse(JSON.stringify(req.query));
-            const getMany = this.rules.access.getMany;
-            const page = parseInt((typeof req.query._page === 'string' && req.query._page) || '1')
-            const pageSize = parseInt((typeof req.query._page_size === 'string' && req.query._page_size) || defaultPageSize)
-            delete filter._page;
-            delete filter._page_size;
-            delete filter._order_by;
-            delete filter._order_by_desc;
-            if (typeof getMany === 'object') {
-                filter[getMany.filter.documentField] = this.user[getMany.filter.variableSession];
-            }
-            if (this.rules.logicalDelete) {
-                filter.delete = false;
-            }
-
-            this.model.find(filter)
-                .skip(pageSize * (page - 1))
-                .limit(pageSize)
-                .then(_res => res.json(_res))
-                .catch(err => res.status(500).end(err.message || err))
-        }
+  /**
+   * Delete a document in the specifict collection
+   * @param req The request sended by the client
+   * @param res The response send to the client
+   * @param _id Id of the document
+   */
+  public delete(req: express.Request, res: express.Response, _id: any) {
+    const filter: Dictionary<any> = { _id };
+    const _delete = this.rules.access.delete;
+    if (typeof _delete === 'object') {
+      filter[_delete.filter.documentField] = this.user[_delete.filter.variableSession];
     }
-
-    /**
-     * Insert a document in the specific collection
-     * @param req The request sended by the client
-     * @param res The response send to the client
-     */
-    public post(req: express.Request, res: express.Response) {
-        const payload = { ...req.body };
-        const post = this.rules.access.post;
-        if (typeof post === 'object') {
-            payload[post.filter.documentField] = this.user[post.filter.variableSession];
-        }
-        if (this.rules.logicalDelete) {
-            payload.delete = false;
-        }
-        this.model.create(payload)
-            .then(_res => res.status(201).json(_res._id))
-            .catch(err => err => res.status(500).end(err.message || err));
+    if (this.rules.logicalDelete) {
+      filter.delete = false;
     }
-
-    /**
-     * Update a document in the specifict collection
-     * @param req The request sended by the client
-     * @param res The response send to the client
-     * @param _id Id of the document
-     */
-    public put(req: express.Request, res: express.Response, _id: any) {
-        const filter: Dictionary<any> = { _id };
-        const payload = { ...req.body };
-        const put = this.rules.access.put;
-        if (typeof put === 'object') {
-            delete payload[put.filter.documentField];
-            filter[put.filter.documentField] = this.user[put.filter.variableSession];
-        }
-        if (this.rules.logicalDelete) {
-            filter.delete = false;
-            payload.delete = false;
-        }
-
-        this.model.update(
-            filter,
-            { $set: payload }
-        ).then(() => res.status(204).end())
-        .catch(err => res.status(500).end(err.message || err));
-    }
-
-    /**
-     * Delete a document in the specifict collection
-     * @param req The request sended by the client
-     * @param res The response send to the client
-     * @param _id Id of the document
-     */
-    public delete(req: express.Request, res: express.Response, _id: any) {
-        const filter: Dictionary<any> = { _id };
-        const _delete = this.rules.access.delete;
-        if (typeof _delete === 'object') {
-            filter[_delete.filter.documentField] = this.user[_delete.filter.variableSession];
-        }
-        if (this.rules.logicalDelete) {
-            filter.delete = false;
-        }
-        this.model.deleteOne(filter)
-            .then(() => res.json(_id))
-            .catch(err => err => res.status(500).end(err.message || err));
-    }
+    this.model.deleteOne(filter)
+      .then(() => res.json(_id))
+      .catch(err => err => res.status(500).end(err.message || err));
+  }
 }
--- src/decorators/model.decorator.ts
+++ src/decorators/model.decorator.ts
@@ -16,58 +16,68 @@ export const modelsCollection: Dictionary<mongoose.Model<any>> = { }
  * @param rules Rules access from the web api
  */
 export function model(name: string, rules?: Rules | undefined) {
-    return function (constructor: Function) {
-        const _rules = Object.assign<Rules, Rules>(rules || {}, {});
+  return function (constructor: Function) {
+    const _rules = Object.assign<Rules, Rules>(rules || {}, {});
 
-        // Set the default values if some rule haven't any
-        if (!_rules.access) {
-            _rules.access = {}
-        }
-
-        _rules.access.getOne = rules?.access?.getOne || 'public';
-        _rules.access.getMany = rules?.access?.getMany || 'public';
-        _rules.access.post = rules?.access?.post || 'public';
-        _rules.access.put = rules?.access?.put || 'public';
-        _rules.access.delete = rules?.access?.delete || 'public';
-
-        // Register the `getOne` document access rules
-        if (_rules.access.getOne != 'disabled') {
-            const url = `/api/${name}/:id`;
-            (typeof _rules.access.getOne === 'object' || _rules.access.getOne == 'private') && app.get(url, handleAuthRequest);
-            app.get(url, (req, res) => handleRequest(req, res, handleGetOneRequest));
-        }
-
-        // Register the `getMany` document access rules
-        if (_rules.access.getMany != 'disabled') {
-            const url = `/api/${name}`;
-            (typeof _rules.access.getMany === 'object' || _rules.access.getMany == 'private') && app.get(url, handleAuthRequest);
-            app.get(url, (req, res) => handleRequest(req, res, handleGetManyRequest));
-        }
-
-        // Register the `post` document access rules
-        if (_rules.access.post != 'disabled') {
-            const url = `/api/${name}`;
-            (typeof _rules.access.post === 'object' || _rules.access.post == 'private') && app.get(url, handleAuthRequest);
-            app.post(url, (req, res) => handleRequest(req, res, handlePostRequest));
-        }
-
-        // Register the `put` document access rules
-        if (_rules.access.put != 'disabled') {
-            const url = `/api/${name}/:id`;
-            (typeof _rules.access.put === 'object' || _rules.access.put == 'private') && app.get(url, handleAuthRequest);
-            app.put(url, (req, res) => handleRequest(req, res, handlePutRequest));
-        }
-
-        // Register the `delete` document access rules
-        if (_rules.access.delete != 'disabled') {
-            const url = `/api/${name}/:id`;
-            (typeof _rules.access.delete === 'object' || _rules.access.delete == 'private') && app.get(url, handleAuthRequest);
-            app.delete(url, (req, res) => handleRequest(req, res, handleDeleteRequest));
-        }
-
-        rulesCollection[name] = _rules;
-        constructor.prototype.schemaName = name;
+    // Set the default values if some rule haven't any
+    if (!_rules.access) {
+      _rules.access = {}
     }
+
+    _rules.access.getOne = rules?.access?.getOne || 'public';
+    _rules.access.getMany = rules?.access?.getMany || 'public';
+    _rules.access.post = rules?.access?.post || 'public';
+    _rules.access.put = rules?.access?.put || 'public';
+    _rules.access.delete = rules?.access?.delete || 'public';
+
+    // Register the `getOne` document access rules
+    if (_rules.access.getOne != 'disabled') {
+      const url = `/api/${name}/:id`;
+      if (typeof _rules.access.getOne === 'object' || _rules.access.getOne == 'private') {
+        app.get(url, handleAuthRequest);
+      }
+      app.get(url, (req, res) => handleRequest(req, res, handleGetOneRequest));
+    }
+
+    // Register the `getMany` document access rules
+    if (_rules.access.getMany != 'disabled') {
+      const url = `/api/${name}`;
+      if (typeof _rules.access.getMany === 'object' || _rules.access.getMany == 'private') {
+        app.get(url, handleAuthRequest);
+      }
+      app.get(url, (req, res) => handleRequest(req, res, handleGetManyRequest));
+    }
+
+    // Register the `post` document access rules
+    if (_rules.access.post != 'disabled') {
+      const url = `/api/${name}`;
+      if (typeof _rules.access.post === 'object' || _rules.access.post == 'private') {
+        app.post(url, handleAuthRequest);
+      }
+      app.post(url, (req, res) => handleRequest(req, res, handlePostRequest));
+    }
+
+    // Register the `put` document access rules
+    if (_rules.access.put != 'disabled') {
+      const url = `/api/${name}/:id`;
+      if (typeof _rules.access.put === 'object' || _rules.access.put == 'private') {
+        app.put(url, handleAuthRequest);
+      }
+      app.put(url, (req, res) => handleRequest(req, res, handlePutRequest));
+    }
+
+    // Register the `delete` document access rules
+    if (_rules.access.delete != 'disabled') {
+      const url = `/api/${name}/:id`;
+      if (typeof _rules.access.delete === 'object' || _rules.access.delete == 'private') {
+        app.delete(url, handleAuthRequest);
+      }
+      app.delete(url, (req, res) => handleRequest(req, res, handleDeleteRequest));
+    }
+
+    rulesCollection[name] = _rules;
+    constructor.prototype.schemaName = name;
+  }
 }
 
 /**
@@ -77,14 +87,14 @@ export function model(name: string, rules?: Rules | undefined) {
  * @param next 
  */
 function handleAuthRequest(req: Request, res: Response, next: () => void) {
-    var tokenDecoded = Session.instance.decodeSession<Dictionary<any>>(req.headers.authorization);
-    if (!tokenDecoded) {
-        res.status(401).end();
-        return;
-    }
+  var tokenDecoded = Session.instance.decodeSession<Dictionary<any>>(req.headers.authorization);
+  if (!tokenDecoded) {
+    res.status(401).end();
+    return;
+  }
 
-    req['tokenDecoded'] = tokenDecoded;
-    next();
+  req['tokenDecoded'] = tokenDecoded;
+  next();
 }
 
 /**
@@ -94,27 +104,27 @@ function handleAuthRequest(req: Request, res: Response, next: () => void) {
  * @param fn Middleware that handle an specific request
  */
 function handleRequest(req: Request, res: Response, fn: (Request, Response, controller: ModelController) => void) {
-    const modelName = req.path.split('/')[2];
-    const modelController = new ModelController(modelsCollection, rulesCollection, req['tokenDecoded'], modelName);
-    fn(req, res, modelController);
+  const modelName = req.path.split('/')[2];
+  const modelController = new ModelController(modelsCollection, rulesCollection, req['tokenDecoded'], modelName);
+  fn(req, res, modelController);
 }
 
 function handleGetOneRequest(req: Request, res: Response, controller: ModelController) {
-    controller.get(req, res, req.params.id);
+  controller.get(req, res, req.params.id);
 }
 
 function handleGetManyRequest(req: Request, res: Response, controller: ModelController) {
-    controller.get(req, res);
+  controller.get(req, res);
 }
 
 function handlePostRequest(req: Request, res: Response, controller: ModelController) {
-    controller.post(req, res);
+  controller.post(req, res);
 }
 
 function handlePutRequest(req: Request, res: Response, controller: ModelController) {
-    controller.put(req, res, req.params.id);
+  controller.put(req, res, req.params.id);
 }
 
 function handleDeleteRequest(req: Request, res: Response, controller: ModelController) {
-    controller.delete(req, res, req.params.id);
+  controller.delete(req, res, req.params.id);
 }
